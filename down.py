ca6156b1990bd2ab3dd5afbfac01eb89c65cd821
Giorgos Tzampanakis <giorgos.tzampanakis@gmail.com>
2016-07-21 17:06:31 +0000

    On and on.

--- down.py
+++ down.py
@@ -1,11 +1,12 @@
-import os, collections, logging, re, heapq, sys
+import os, collections, logging, re, heapq, sys, sqlite3
+import json
 import downloader
 
 _LOGGER = logging.getLogger(__name__)
 
 ROOT_DIR = os.path.dirname(__file__)
 
-MATCHES_LIMIT = 500000
+MATCHES_LIMIT = 400000
 
 YEARS = range(2015, 2003, -1)
 STALE = 9999
@@ -16,8 +17,49 @@ PLAYER_RE = re.compile(r'^[a-z0-9A-F%-]{2,}.\d{1,}$')
 
 matches = [ ]
 
+def get_conn():
+	conn = sqlite3.connect(os.path.join(ROOT_DIR, 'matches.db'))
+	conn.executescript('''
+		create table if not exists matches(
+			id integer primary key autoincrement,
+			date text not null,
+			team1 text not null,
+			team2 text not null,
+			match_json text not null
+		);
+		create index if not exists matches_date on matches(date);
+		create unique index if not exists uniqueness1 on matches(
+												team1, team2, date);
+	''')
+	return conn
+
+conn = None
+def store_match(match_dict):
+	global conn
+	if conn is None:
+		conn = get_conn()
+	try:
+		conn.execute('''
+			insert or ignore into matches
+			(date, team1, team2, match_json)
+			values
+			(?, ?, ?, ?)
+		''', [
+			match_dict['date'],
+			match_dict['teams'][0],
+			match_dict['teams'][1],
+			json.dumps(match_dict)
+		])
+	except Exception as e:
+		conn.rollback()
+		raise
+	finally:
+		conn.commit()
+
 def comps_handler(crawl_element):
+
 	result = collections.defaultdict(list)
+
 	elems = crawl_element.xpath(
 		"//div[@id='yw1']//td[@class='hauptlink']//a[1][@title]",
 		[5, None]
@@ -32,12 +74,60 @@ def comps_handler(crawl_element):
 	for page_elem in page_elems:
 		result[comps_handler].append(downloader.CrawlURL(
 								page_elem.element.get('href'), STALE))
+
+	urls = []
+
+	# CHL
+	urls += [
+		downloader.CrawlURL(
+			'http://www.transfermarkt.com/uefa-champions-league'
+			'/gesamtspielplan/pokalwettbewerb/CL/saison_id/%s' % year,
+			STALE
+		)
+		for year in YEARS
+	]
+
+	# CHL qual
+	urls += [
+		downloader.CrawlURL(
+			'http://www.transfermarkt.com/uefa-cl-qualifikation'
+			'/gesamtspielplan/pokalwettbewerb/CLQ/saison_id/%s' % year,
+			STALE
+		)
+		for year in YEARS
+	]
+
+	# EUL
+	urls += [
+		downloader.CrawlURL(
+			'http://www.transfermarkt.com/europa-league'
+			'/gesamtspielplan/pokalwettbewerb/EL/saison_id/%s' % year,
+			STALE
+		)
+		for year in YEARS
+	]
+
+	# EUL qual
+	urls += [
+		downloader.CrawlURL(
+			'http://www.transfermarkt.com/europa-league-qualifikation'
+			'/gesamtspielplan/pokalwettbewerb/ELQ/saison_id/%s' % year,
+			STALE
+		)
+		for year in YEARS
+	]
+
+	for url in urls:
+		result[comp_handler].append(url)
+
 	return result
 
 def comp_handler(crawl_element):
 	result = collections.defaultdict(list)
 	elems = crawl_element.xpath("//a[@title='Match report']")
 	for elemi, elem in enumerate(elems):
+		if '-:-' in elem.text_content():
+			continue
 		href = elem.element.get('href')
 		result[match_handler].append(downloader.CrawlURL(href, STALE))
 	return result
@@ -49,7 +139,8 @@ def match_handler(crawl_element):
 		return
 
 	date_elem = crawl_element.xpath_one(
-		"//div/div/div/div/p[@class='sb-datum hide-for-small']/a[2]"
+		"//div/div/div/div/p[@class='sb-datum hide-for-small']"
+		"/a[contains(@href, '/datum')]"
 	)
 	href = date_elem.element.get('href')
 	mo = DATE_RE.search(href)
@@ -82,9 +173,18 @@ def match_handler(crawl_element):
 	score_text = score_elem.text_content().strip()
 	score_mo = SCORE_RE.match(score_text)
 	if not score_mo:
-		raise Exception(score_text)
-	score1 = score_mo.group(1)
-	score2 = score_mo.group(2)
+		if (
+			'PENS' in score_elem.text_content() 
+				or 
+			'AET' in score_elem.text_content()
+				or
+			'-:-' in score_elem.text_content()
+		):
+			return
+		else:
+			raise Exception(score_text)
+	score1 = int(score_mo.group(1))
+	score2 = int(score_mo.group(2))
 
 	lineups = [[], []]
 	try:
@@ -120,13 +220,9 @@ def match_handler(crawl_element):
 				assert PLAYER_RE.match(player_id), player_id
 				lineups[team_index].append(player_id)
 	except downloader.UnexpectedContentException as e:
-		if player_index > 1:
-# There are cases like [1] where fewer than 11 players are listed. These are
-# cases we should just skip.
-# [1] https://tinyurl.com/jy2cwhx
-			return
-		else:
-			raise
+		# There are cases where lineups are not available, for example when the
+		# match was uncontested. Example: https://tinyurl.com/h6da9uu
+		return 
 
 	assert lineups[0] != lineups[1]
 	match = {
@@ -135,31 +231,16 @@ def match_handler(crawl_element):
 		'score': [score1, score2],
 		'lineups': lineups,
 	}
+	store_match(match)
 	matches.append(match)
 	if len(matches) % 25 == 0:
 		print 'Matches so far:', len(matches)
 		print 'Last match:', matches[-1]
 	
 	if len(matches) >= MATCHES_LIMIT:
-		export()
 		sys.exit(0)
 
 	return
-
-def export():
-	matches.sort(key = lambda ma: ma['date'])
-	with open('matches.csv', 'wb') as outf:
-		for ma in matches:
-			outf.write(';'.join([
-				ma['date'],
-				ma['teams'][0],
-				ma['teams'][1],
-				ma['score'][0],
-				ma['score'][1],
-				','.join(ma['lineups'][0]),
-				','.join(ma['lineups'][1]),
-			]))
-			outf.write('\n')
 
 def crawl():
 	db_path = os.path.join(ROOT_DIR, 'pages.db')
@@ -167,9 +248,9 @@ def crawl():
 	crawler = downloader.Crawler(
 		comps_handler,
 		downloader.CrawlURL(
-			'http://www.transfermarkt.com/?seo=wettbewerbe&plus=1', STALE),
+			'http://www.transfermarkt.com/?ajax=yw1&page=2&plus=1&seo=wettbewerbe', STALE),
 		db_path,
-		[.2, 5]
+		[.2, 4]
 	)
 	crawler.crawl()
 
--- eval.py
+++ eval.py
@@ -1,43 +1,62 @@
-"""
-An ELO system needs:
-
-a) f(c) -> r, where c is a contestant and r his rating.
-b) g(d) -> scd, where d is a rating difference and scd is a score distribution.
-"""
-import collections, uuid
+import collections, uuid, json, sqlite3, os
 
 import util
 
 DEFAULT_RATING = 0.
 
-NCHUNKS = 40
+NCHUNKS = 25
+
+ROOT_DIR = os.path.dirname(__file__)
+
+PLAYER_TO_TRACK = 'cristiano-ronaldo'
 
 K = .05
 H = .35
+T = 20
+M = 1
 
 matches = [ ]
 from_id = { }
-with open('matches.froz.csv') as inf:
-	for row in inf:
-		row = row.strip()
-		date, team1, team2, score1, score2, lineup1, lineup2 = row.split(';')
-		lineup1 = lineup1.split(',')
-		lineup2 = lineup2.split(',')
-		ma = dict(
-			id = uuid.uuid4().hex,
-			date = date,
-			teams = [team1, team2],
-			team1 = team1,
-			team2 = team2,
-			scores = [int(score1), int(score2)],
-			score1 = int(score1),
-			score2 = int(score2),
-			lineups = [lineup1, lineup2],
-			lineup1 = lineup1,
-			lineup2 = lineup2,
-		)
-		matches.append(ma)
-		from_id[ma['id']] = ma
+# with open('matches.froz.csv') as inf:
+# 	for row in inf:
+# 		row = row.strip()
+# 		date, team1, team2, score1, score2, lineup1, lineup2 = row.split(';')
+# 		lineup1 = lineup1.split(',')
+# 		lineup2 = lineup2.split(',')
+# 		ma = dict(
+# 			id = uuid.uuid4().hex,
+# 			date = date,
+# 			teams = [team1, team2],
+# 			team1 = team1,
+# 			team2 = team2,
+# 			scores = [int(score1), int(score2)],
+# 			score1 = int(score1),
+# 			score2 = int(score2),
+# 			lineups = [lineup1, lineup2],
+# 			lineup1 = lineup1,
+# 			lineup2 = lineup2,
+# 		)
+# 		matches.append(ma)
+# 		from_id[ma['id']] = ma
+
+def get_conn():
+	conn = sqlite3.connect(os.path.join(ROOT_DIR, 'matches.db'))
+	return conn
+
+conn = get_conn()
+
+results = conn.execute('''
+	select id, match_json
+	from matches
+	order by date
+''')
+
+for row in results:
+	ma_id = row[0]
+	ma = json.loads(row[1])
+	ma['id'] = ma_id
+	from_id[ma_id] = ma
+	matches.append(ma)
 
 rmap = collections.defaultdict(lambda: DEFAULT_RATING)
 total_error = 0.
@@ -48,31 +67,43 @@ for ma in matches:
 	teams_ratings = [sum(rr) for rr in ratings]
 
 	exp_sup = teams_ratings[0] - teams_ratings[1] + H
-	print ma['teams'], ma['date'], exp_sup
-	obs_sup = ma['scores'][0] - ma['scores'][1]
+	print (
+		'%s %s %.3f-%.3f %.3f' % (
+			ma['teams'], ma['date'], teams_ratings[0], teams_ratings[1], exp_sup
+		)
+	)
+	obs_sup = ma['score'][0] - ma['score'][1]
 
 	error = obs_sup - exp_sup
 	total_error += abs(error)
 	correction = error * K
 	correction_per_player = correction / 11.
 
+	ma['sum_team_ratings'] = sum(teams_ratings)
+	ma['team_ratings'] = teams_ratings
 	ma['exp_sup'] = exp_sup
 	ma['obs_sup'] = obs_sup
-	ma['error'] = error
+	ma['abs_error'] = abs(error)
+	ma['abs_error_of_dumb'] = abs(obs_sup - H)
 
 	for p in ma['lineups'][0]:
+		if p.split('.')[0] == PLAYER_TO_TRACK:
+			print 'TRACKING (%s): %.3f' % (p.split('.')[0], rmap[p])
 		rmap[p] += correction_per_player
 	for p in ma['lineups'][1]:
+		if p.split('.')[0] == PLAYER_TO_TRACK:
+			print 'TRACKING (%s): %.3f' % (p.split('.')[0], rmap[p])
 		rmap[p] -= correction_per_player
 
 for p in sorted(rmap.keys(), key = lambda p: -rmap[p])[:1500]:
-	print '%s    %.2f' % (p.split('.')[0], rmap[p])
+	print '%s    %.3f' % (p.split('.')[0], rmap[p])
 
 print 'Total error: %s' % total_error
 
 chunk_size = len(matches) / NCHUNKS
 
-to_chunkify = sorted([obj for obj in matches], 
+
+to_chunkify = sorted(matches, 
 					 key = lambda ma: ma['exp_sup'])
 to_chunkify = [obj['id'] for obj in to_chunkify]
 
@@ -81,11 +112,25 @@ chunks = util.chunkify(to_chunkify, chunk_size)
 print
 for chunk in chunks:
 	avg_exp = util.avg([from_id[id]['exp_sup'] for id in chunk])
-	avg_obs = util.avg([from_id[id]['obs_sup'] for id in chunk])
-	std_obs = util.std([from_id[id]['obs_sup'] for id in chunk])
-	avg_err = util.avg([from_id[id]['error'] for id in chunk])
+	avg_err_d = util.avg([from_id[id]['abs_error_of_dumb'] for id in chunk])
+	avg_err = util.avg([from_id[id]['abs_error'] for id in chunk])
 	n = len(chunk)
-	to_print = '%s %.3f %.3f %.3f %.3f' % (n, avg_exp, avg_obs, 
-								 		   std_obs, avg_err/std_obs)
+	to_print = '%s %.3f %.3f %.3f' % (n, avg_exp, avg_err_d, avg_err)
+	print to_print
+
+
+to_chunkify = sorted(matches, 
+					 key = lambda ma: ma['sum_team_ratings'])
+to_chunkify = [obj['id'] for obj in to_chunkify]
+
+chunks = util.chunkify(to_chunkify, chunk_size)
+
+print
+for chunk in chunks:
+	avg_sum = util.avg([from_id[id]['sum_team_ratings'] for id in chunk])
+	avg_err_d = util.avg([from_id[id]['abs_error_of_dumb'] for id in chunk])
+	avg_err = util.avg([from_id[id]['abs_error'] for id in chunk])
+	n = len(chunk)
+	to_print = '%s %.3f %.3f %.3f' % (n, avg_sum, avg_err_d, avg_err)
 	print to_print
 
