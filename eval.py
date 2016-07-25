d3c495d94838cb305fcf57270a976a938ff71ebd
Giorgos Tzampanakis <giorgos.tzampanakis@gmail.com>
2016-07-25 08:58:06 +0300

    Classified. Preparing cross-validation.

--- eval.py
+++ eval.py
@@ -9,74 +9,149 @@ DEFAULT_RATING = 0.
 
 PS_PER_TEAM = 11
 
-NCHUNKS = 25
-
 ROOT_DIR = os.path.dirname(__file__)
 
-PLAYER_TO_TRACK = 'gyorgy-sandor'
+class Evaluator:
 
-N = 500000
+	def __init__(self, matches, **pars):
+		self.matches = matches
+		for key, val in pars.iteritems():
+			setattr(self, key, val)
+		self.pars = pars
 
-K = .05
-H = .35
-T = 20
-M = 1
+	def evaluate(self):
 
-matches = [ ]
-from_id = { }
-# with open('matches.froz.csv') as inf:
-# 	for row in inf:
-# 		row = row.strip()
-# 		date, team1, team2, score1, score2, lineup1, lineup2 = row.split(';')
-# 		lineup1 = lineup1.split(',')
-# 		lineup2 = lineup2.split(',')
-# 		ma = dict(
-# 			id = uuid.uuid4().hex,
-# 			date = date,
-# 			teams = [team1, team2],
-# 			team1 = team1,
-# 			team2 = team2,
-# 			scores = [int(score1), int(score2)],
-# 			score1 = int(score1),
-# 			score2 = int(score2),
-# 			lineups = [lineup1, lineup2],
-# 			lineup1 = lineup1,
-# 			lineup2 = lineup2,
-# 		)
-# 		matches.append(ma)
-# 		from_id[ma['id']] = ma
+		N = len(self.matches)
 
-def get_conn():
-	conn = sqlite3.connect(os.path.join(ROOT_DIR, 'matches.db'))
-	return conn
+		H = self.pars['H']
 
-conn = get_conn()
+		matches = self.matches
 
-results = conn.execute('''
-	select id, match_json from (
-		select id, match_json, date
-		from matches
-		order by date desc
-		limit ?
-	)
-	order by date
-''', [N])
+		plist = [ ]
+		p2i = { }
+		i2p = { }
+		rlist = [ ]
+		obssuplist = [ ]
+		p2e = collections.defaultdict(int)
 
-for row in results:
-	ma_id = row[0]
-	ma = json.loads(row[1])
-	ma['id'] = ma_id
-	from_id[ma_id] = ma
-	matches.append(ma)
+		for mai, ma in enumerate(matches):
+
+			for p in itertools.chain(*ma['lineups']):
+				p2e[p] += 1
+				if p not in p2i:
+					i = len(plist)
+					plist.append(p)
+					rlist.append(DEFAULT_RATING)
+					p2i[p] = i
+					i2p[i] = p
+
+			obs_sup = ma['score'][0] - ma['score'][1]
+			obssuplist.append(obs_sup)
+
+# Column vector of ratings.
+		R = sp.array(rlist)
+
+# Column vector of observed superiorities.
+		SO = sp.array(obssuplist)
+
+# Matrix of appearances. Columns are players, rows are matches.
+# First using lil_matrix because it allows to build incrementally.
+		A = sps.lil_matrix((N, R.size))
+
+
+		for mai, ma in enumerate(matches):
+			for li, l in zip([1,-1], ma['lineups']):
+				assert len(l) == PS_PER_TEAM
+				assert len(set(l)) == PS_PER_TEAM
+				for p in l:
+					A[mai, p2i[p]] = li
+
+# Now convert to CSC format for faster operations.
+		A = A.tocsr()
+
+		HM = sp.ones(N) * H
+
+		assert A.sum() == 0
+
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+		res = spsl.lsqr(A, SO - HM, damp=N/1000., show=True)
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+		##############################################
+
+		R = res[0]
+		rnorm = res[3]
+		err = rnorm / N
+
+		inds = np.argsort(R)
+
+		nprinted = 0
+		for ind in reversed(inds):
+			exp = p2e[i2p[ind]]
+			if exp >= 0:
+				print '%s    %d    %.3f' % (i2p[ind], exp, R[ind])
+				nprinted += 1
+			if nprinted >= 100:
+				break
+
+		output = { }
+		p2r = { }
+		
+		for p, i in p2i.iteritems():
+			p2r[p] = R[p2i[p]]
+
+		output['err'] = err
+		output['p2r'] = p2r
+
+		return output
+
+def main():
+
+	lim = 1000
+
+	pK = .05
+	pH = .35
+
+	matches = [ ]
+
+	def get_conn():
+		conn = sqlite3.connect(os.path.join(ROOT_DIR, 'matches.db'))
+		return conn
+
+	conn = get_conn()
+
+	results = conn.execute('''
+		select id, match_json from (
+			select id, match_json, date
+			from matches
+			order by date desc
+			limit ?
+		)
+		order by date
+	''', [lim])
+
+	for row in results:
+		ma_id = row[0]
+		ma = json.loads(row[1])
+		ma['id'] = ma_id
+		matches.append(ma)
 
 # Discard all matches that do not have 22 distinct players, for whatever
 # reason.
-matches = [
-	m for m in matches
-	if  len(set(
-		m['lineups'][0] + m['lineups'][1]
-	)) == PS_PER_TEAM * 2
-]
+	matches = [
+		m for m in matches
+		if  len(set(
+			m['lineups'][0] + m['lineups'][1]
+		)) == PS_PER_TEAM * 2
+	]
 
 # matches = [
 # 		{
@@ -89,81 +164,9 @@ matches = [
 # 		},
 # ]
 
-N = len(matches)
 
-plist = [ ]
-p2i = { }
-i2p = { }
-rlist = [ ]
-obssuplist = [ ]
-p2e = collections.defaultdict(int)
+	evaluator = Evaluator(matches, H = pH)
+	return evaluator.evaluate()
 
-for mai, ma in enumerate(matches):
-
-	for p in itertools.chain(*ma['lineups']):
-		p2e[p] += 1
-		if p not in p2i:
-			i = len(plist)
-			plist.append(p)
-			rlist.append(DEFAULT_RATING)
-			p2i[p] = i
-			i2p[i] = p
-
-	obs_sup = ma['score'][0] - ma['score'][1]
-	obssuplist.append(obs_sup)
-
-# Column vector of ratings.
-R = sp.array(rlist)
-
-# Column vector of observed superiorities.
-SO = sp.array(obssuplist)
-
-# Matrix of appearances. Columns are players, rows are matches.
-# First using lil_matrix because it allows to build incrementally.
-A = sps.lil_matrix((N, R.size))
-
-
-for mai, ma in enumerate(matches):
-	for li, l in zip([1,-1], ma['lineups']):
-		assert len(l) == PS_PER_TEAM
-		assert len(set(l)) == PS_PER_TEAM
-		for p in l:
-			A[mai, p2i[p]] = li
-
-# Now convert to CSC format for faster operations.
-A = A.tocsr()
-
-HM = sp.ones(N) * H
-
-assert A.sum() == 0
-
-##############################################
-##############################################
-##############################################
-##############################################
-##############################################
-##############################################
-res = spsl.lsqr(A, SO - HM, damp=N/1000., show=True)
-##############################################
-##############################################
-##############################################
-##############################################
-##############################################
-##############################################
-
-R = res[0]
-
-inds = np.argsort(R)
-
-nprinted = 0
-for ind in reversed(inds):
-	exp = p2e[i2p[ind]]
-	if exp >= 0:
-		print '%s    %d    %.3f' % (i2p[ind], exp, R[ind])
-		nprinted += 1
-	if nprinted >= 5000:
-		break
-
-print 'N', N
-
-
+if __name__ == '__main__':
+	main()
